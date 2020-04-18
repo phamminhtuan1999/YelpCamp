01e2c92ef89a0a04fdc4eb84af3b22111f5ec7f5
phamminhtuan1999 <phamminhtuan1999@gmail.com>
2020-04-19 00:20:20 +0700

    v1 done

--- v1/app.js
+++ v1/app.js
@@ -1,31 +1,45 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+  {
+    name: "Cat",
+    image:
+      "https://en.freejpg.com.ar/image-900/8a/8ad1/F100011956-gray_cat_with_yellow_eyes_looking_at_the_camera.jpg",
+  },
+  {
+    name: "Dog",
+    image:
+      "https://en.freejpg.com.ar/image-900/12/1247/F100011831-close_up_of_a_dog_sitting_on_the_grass_with_a_natural_background.jpg",
+  },
+  {
+    name: "Cow",
+    image:
+      "https://en.freejpg.com.ar/image-900/91/91ad/F100011815-close_up_of_a_brown_cow_with_a_blue_sky_in_the_background.jpg",
+  },
+];
 
 app.get("/", function (req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function (req, res) {
-  var campgrounds = [
-    {
-      name: "Cat",
-      image:
-        "https://en.freejpg.com.ar/image-900/8a/8ad1/F100011956-gray_cat_with_yellow_eyes_looking_at_the_camera.jpg",
-    },
-    {
-      name: "Dog",
-      image:
-        "https://en.freejpg.com.ar/image-900/12/1247/F100011831-close_up_of_a_dog_sitting_on_the_grass_with_a_natural_background.jpg",
-    },
-    {
-      name: "Cow",
-      image:
-        "https://en.freejpg.com.ar/image-900/91/91ad/F100011815-close_up_of_a_brown_cow_with_a_blue_sky_in_the_background.jpg",
-    },
-  ];
   res.render("campgrounds", { campgrounds: campgrounds });
+});
+
+app.post("/campgrounds", function (req, res) {
+  var newName = req.body.name;
+  var newImage = req.body.image;
+  campgrounds.push({ name: newName, image: newImage });
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function (req, res) {
+  res.render("new");
 });
 
 app.listen(process.env.PORT || 3000, function () {
